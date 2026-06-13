8be87a8effdd7c2dc5d14c7825cf73493ce735ad
chicuong2k3 <nguye@DESKTOP-TIJS8A2>
2026-06-13 19:40:48 +0700

    @ feat: add BlazorWasmPreRendering.Build and GitHub Pages CI/CD
    - Install BlazorWasmPreRendering.Build 8.0.0 for instant first paint
    - Refactor Program.cs to use ConfigureServices static local function
    - Add .github/workflows/docs.yml to deploy prerendered docs to GitHub Pages
    - Configure prerendering: delete loading contents, append .html extension
    @

--- docs/GuideFlow.Docs/Program.cs
+++ docs/GuideFlow.Docs/Program.cs
@@ -4,10 +4,20 @@ using GuideFlow.Services;
 using GuideFlow.Docs;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddGuideFlow();
+// Avoid duplicate root components during prerendering
+if (!builder.RootComponents.Any())
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+}
+
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+    services.AddGuideFlow();
+}
